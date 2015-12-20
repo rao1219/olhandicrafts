34cd39b2e04ae2a28a0a97555df5f5cd3b64fd0f
rao1219 <441718399@qq.com>
2015-12-20 23:00:14 +0800

    add robot

---
+++ robot.php
@@ -0,0 +1,30 @@
+<?php
+session_start();
+?>  <!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=Edge">
+    <title>自动客服 - Handicrafts</title>
+<?php
+include 'templete\header.html';
+?>
+<link rel="stylesheet" href="style/css/newslist.css">
+<div class="fl wfs bcf7" style="padding-bottom:20px;">
+        <div class="regist-process-wrapper">
+            <div class="nearby-process-body fl wfs">
+
+<div class="blank"></div>
+<iframe  width=1200 height=700 src="jquery-html5/chat.html">
+</div></div></div>
+
+            <div class="blank"></div>
+
+     <div class="cover-page-index fl wfs bcf2" style="padding-top:20px;">
+        <div class="cover-page-wrapper">
+        </div>
+
+
+ 
+</body>
+</html>		
